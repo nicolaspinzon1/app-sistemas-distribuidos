39f1abe65ae2d73351e87206aea2a1b4a406d5f1
Nicolas pinzon <nicolasnnnnn7@gmail.com>
2023-05-24 21:49:19 +0200

    added curso model

--- curso/models.py
+++ curso/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Curso(models.Model):
+    idCurso = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=64, blank=False, null=False)
+    descripcion = models.CharField(max_length=20,  blank=True, null=True)
+    duracion = models.DurationField()
+    fechaInicio = models.DateTimeField(auto_now_add=True)
+    fechaFinal = models.DateTimeField(null= True, blank=True)
+
+    class Meta:
+        verbose_name = "Curso"
+        verbose_name_plural = "Cursos"
+
+    def __str__(self):
+        return f"{self.nombre}  "  
