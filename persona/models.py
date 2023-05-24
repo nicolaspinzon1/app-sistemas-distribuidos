ebeaf924518f8b7519f5e959eb19a24c9d8529a3
Nicolas pinzon <nicolasnnnnn7@gmail.com>
2023-05-24 21:52:12 +0200

    added persona model

--- persona/models.py
+++ persona/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from rol.models import Rol
 
-# Create your models here.
+
+
+class Persona(models.Model):
+    idPersona = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=32, blank=False, null=False)
+    apellido = models.CharField(max_length=32,  blank=True, null=True)
+    cedula = models.IntegerField()
+    telefono =models.IntegerField()
+    Rol_idRol = models.ForeignKey(Rol, on_delete=models.CASCADE)
+
+    
+    class Meta:
+        verbose_name = "Persona"
+        verbose_name_plural = "Personas"
+
+    def __str__(self):
+        return f"{self.nombre}  {self.apellido}"  
