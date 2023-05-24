947c8ac7e78082b0f941a6fce1f6fc70dc8ad0b5
Nicolas pinzon <nicolasnnnnn7@gmail.com>
2023-05-24 21:52:28 +0200

    added rol model

--- rol/models.py
+++ rol/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Rol(models.Model):
+    idRol = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=16, blank=False, null=False)
+    descripcion = models.CharField(max_length=64,  blank=True, null=True)
+    
+    class Meta:
+        verbose_name = "Rol"
+        verbose_name_plural = "Roles"
+
+    def __str__(self):
+        return f"{self.nombre}  "  
